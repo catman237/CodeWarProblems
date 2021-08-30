7d108c11f3edd06b135e6b8454a8faed6c8d6317
Greg <Ghughes610@gmail.com>
2021-08-30 07:40:40 -0600

    answered

---
+++ question6.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+//  8kyu
+
+
+const booleanToString = b => `${b}`
