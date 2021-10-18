96383cb90c55fe163f60d9ea9c0aa7e17ceac1e1
Greg <Ghughes610@gmail.com>
2021-10-18 09:31:33 -0600

    answered

---
+++ question31.js
@@ -0,0 +1,20 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// 7 kyu 
+
+function solution(str, ending){
+  
+    const position = [str.length - ending.length]
+    
+    if(str.slice(position) === ending) {
+      return true
+    } else {
+      return false
+    }
+    
+  }
