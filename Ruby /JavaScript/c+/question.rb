03260c47183f2933b05d02037cf1abcd9949fc5e
Greg <Ghughes610@gmail.com>
2021-10-31 21:53:59 -0600

    answered

---
+++ Ruby /JavaScript/c+/question.rb
@@ -0,0 +1,5 @@
+# Very simple, given a number, find its opposite.
+
+def opposite n 
+    return -n
+end
--- Ruby /JavaScript/c+/question38.rb
+++ Ruby /JavaScript/c+/question38.rb
@@ -1,4 +1,4 @@
-
+# return Yes if the bool is true and No if it is false
 
 def bool_to_word bool
     bool ? "Yes" : "No"
