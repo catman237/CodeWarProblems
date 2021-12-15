1ff018b0ddd808528d711e204b7094c3fa55ee52
Greg Hughes <58540291+catman237@users.noreply.github.com>
2021-12-15 08:53:47 -0700

    answered

---
+++ Elixir/question2.js
@@ -0,0 +1,11 @@
+# Write a function called repeatStr which repeats the given string string exactly n times.
+
+# repeat_str(6, "I") # "IIIIII"
+# repeat_str(5, "Hello") # "HelloHelloHelloHelloHello"
+
+
+defmodule Solution do
+  def repeat_str do
+    str = String.duplicate(s, n)
+  end
+end
