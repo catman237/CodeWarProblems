3b1658740f5a3489ea8d480f6a7ab894bec8c4d0
Greg Hughes <58540291+catman237@users.noreply.github.com>
2021-12-25 21:55:21 -0700

    answered

---
+++ Elixir/question7.ex
@@ -0,0 +1,21 @@
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+# Examples
+# make_negative 1    # return -1
+# make_negative -5   # return -5
+# make_negative 0    # return 0
+# Notes
+# The number can be negative already, in which case no change is required.
+# Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+# 8 kyu
+
+defmodule Negator do
+  def make_negative(num) do
+    cond do
+      num == 0 -> 0
+      num >= 1 -> String.to_integer("-#{num}")
+      num < 0 -> num
+    end
+  end
+end
---
+++ Elixir/question8.ex
@@ -0,0 +1,12 @@
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# 8 kyu
+
+defmodule Solution do
+  def reverse(string) do
+    String.reverse(string)
+  end
+end
